a924bffeb031526417809d2d678cdb72b8c97f18
mrayandutta@gmail.com <mrayandutta@gmail.com>
2018-05-06 10:56:12 +0530

    Added Changes

---
+++ src/test/scala/characterstreaming/CharacterStreamingTestWithSparkTestingTest.scala
@@ -0,0 +1,18 @@
+package characterstreaming
+
+import com.holdenkarau.spark.testing.StreamingSuiteBase
+import org.scalatest.FunSuite
+
+
+class CharacterStreamingTestWithSparkTestingTest extends FunSuite with StreamingSuiteBase {
+
+  test("capitalize by window") {
+    val input = List(List('a'), List('b'), List('c'), List('d'), List('e'))
+
+    val slide1 = List('A', 'B')
+    val slide2 = List('B', 'C', 'D')
+    val expected = List(slide1, slide2)
+
+    testOperation(input, StreamOperations.capitalizeWindowed, expected)
+  }
+}
